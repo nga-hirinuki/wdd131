163114bb87a87249efc45b49d5c8c64c4d115329
ngahirinuki <hir23003@byui.edu>
2025-05-19 23:46:09 +1200

    submitt Bom activity

--- week02/scripts/loops.js
+++ week02/scripts/loops.js
@@ -2,18 +2,49 @@ const DAYS = 6;
 const LIMIT = 30;
 let studentReport = [11, 42, 33, 64, 29, 37, 44];
 
-//for loop
-// for (let i = 0; i< studentReport.length; i++) {
-//     if (studentReport[i] > LIMIT) {
-//         console.log(studentReport[i]);
-//     }
-// }
+//for loop syntax
 
-// While loop
+// for (initialization; condition; afterthought)
+//  statement
 
-let i = 0;
-while (i < studentReport.length){
-    if (studentReport[i] > LIMIT){
+for (let i = 0; i< studentReport.length; i++) {
+    if (studentReport[i] > LIMIT) {
         console.log(studentReport[i]);
     }
 }
+
+// While loop syntax
+// while (condition)
+//     statement
+
+let i = 0; // declare variable i
+
+while (i < studentReport.length){ 
+    if (studentReport[i]> LIMIT){
+        console.log(studentReport[i]);
+    }
+}
+
+//for each loop syntax
+
+// array.forEach(function(element, index, array) {
+//     // code to run on each element
+//   });
+
+studentReport.forEach(function(item){
+    if (score > LIMIT){
+        console.log(item);
+        }
+    })
+
+// for ... in loop syntax
+// for (variable in object) {
+//     // code to run on each property
+//   }
+
+for(let items in studentReport){
+    if (studentReport[items] > LIMIT)
+        console.log(studentReport[items]);
+    }  
+
+  
